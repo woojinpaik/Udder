b6e1f3ddabec4fd553ea750024b634973014becc
Woojin Paik <wjpaik@kku.ac.kr>
2019-05-09 23:03:40 +0900

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by PaikWoojin on 09/05/2019.
+//  Copyright © 2019 PaikWoojin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
